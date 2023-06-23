b1fa97d1e508a001df92972a1636eb34595198e8
csw180 <swkao@kakao.com>
2023-06-24 07:18:22 +0900

    Revert "Revert "permutaion consumer 기능추가""
    This reverts commit 6be02deeb733a11305afa666395a60f8a28e2ca1.

---
+++ src/bj/bj1850/Main.java
@@ -0,0 +1,34 @@
+// 백준 최대공약수:1850 https://www.acmicpc.net/problem/1850
+
+package bj.bj1850;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    
+    long GCD(long a, long b) {
+        if(b==0) return a;
+        return GCD(b, a%b);
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            long[] ab  = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            Main main = new Main();
+            long  count = main.GCD(ab[0], ab[1]);
+            for(long i=0; i< count; i++ )
+                bw.write("1");
+            bw.flush();
+            bw.close();
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
